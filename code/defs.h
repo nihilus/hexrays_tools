07d2238a08c4498a5759fe9b72d2cd1bc1cfcc2f
Wingkou Loeng <wingkou@outlook.com>
2019-03-16 21:14:47 +0800

    correct HIBYTE(x), HIWORD(x), HIDWORD(x)

--- code/defs.h
+++ code/defs.h
@@ -74,9 +74,9 @@ typedef int bool;       // we want to use bool in our C programs
 #define LOBYTE(x)   (*((_BYTE*)&(x)))   // low byte
 #define LOWORD(x)   (*((_WORD*)&(x)))   // low word
 #define LODWORD(x)  (*((_DWORD*)&(x)))  // low dword
-#define HIBYTE(x)   (*((_BYTE*)&(x)+1))
-#define HIWORD(x)   (*((_WORD*)&(x)+1))
-#define HIDWORD(x)  (*((_DWORD*)&(x)+1))
+#define HIBYTE(x)   (*((_BYTE*)(&(x)+1)-1))
+#define HIWORD(x)   (*((_WORD*)(&(x)+1)-1))
+#define HIDWORD(x)  (*((_DWORD*)(&(x)+1)-1))
 #define BYTEn(x, n)   (*((_BYTE*)&(x)+n))
 #define WORDn(x, n)   (*((_WORD*)&(x)+n))
 #define BYTE1(x)   BYTEn(x,  1)         // byte 1 (counting from 0)
